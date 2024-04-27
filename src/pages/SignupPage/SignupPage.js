d6e9cc571c1ae191ead0629a8881398b78473f3a
Vovka1602 <vovkalash2004@gmail.com>
2024-04-27 23:15:47 +0200

    Login/Signup: Add registration logic

--- src/pages/SignupPage/SignupPage.js
+++ src/pages/SignupPage/SignupPage.js
@@ -1,16 +1,49 @@
+import { useState } from "react";
 import "./SignupPage.css"
+import { useNavigate } from "react-router-dom";
 
 const SignupPage = () => {
-    return ( 
+    const [id, changeId] = useState("");
+    const [password, changePassword] = useState("");
+    const admin_access = false;
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let signup_user = { id, password, admin_access };
+        console.log(signup_user);
+        fetch("http://localhost:8000/users")
+            .then((res) => res.json())
+            .then((response) => {
+                if(response.findIndex(item => signup_user.id === item.id) === -1) {
+                    fetch("http://localhost:8000/users", {
+                        method: "POST",
+                        headers: { 'content-type': 'application/json' },
+                        body: JSON.stringify(signup_user)
+                    })
+                        .then((res) => {
+                            navigate('/login');
+                        })
+                        .catch((err) => {
+                            console.error(err.message);
+                        });
+                } else {
+                    alert(`Логін '${signup_user.id}' уже існує.\nУвійдіть або придумайте інший логін.`)
+                }
+            });
+    }
+
+    return (
         <div className="container">
-            <form className="container">
+            <form className="container" onSubmit={handleSubmit}>
                 <h1>Створити обліковий запис</h1>
-                <input placeholder="Логін"></input>
-                <input placeholder="Пароль" type="password"></input>
+                <input required placeholder="Логін" value={id} onChange={e => changeId(e.target.value)}></input>
+                <input required placeholder="Пароль" value={password} onChange={e => changePassword(e.target.value)} type="password"></input>
                 <button type="submit">Зареєструватися</button>
             </form>
         </div>
     );
 }
- 
+
 export default SignupPage;
